68e57688ca6214d80dee1b8f38a23aaf781e80fd
vcometa <vcometa@postmedia.com>
2013-08-23 17:09:19 -0400

    added browser detection

--- js/main.js
+++ js/main.js
@@ -16,6 +16,18 @@ var weblabs, _w;
 	
 		params: {},
 		
+		userAgent:{
+		
+			value: navigator.userAgent,
+		
+			type: navigator.userAgent.match(/(iPad)|(iPhone)|(iPod)|(android)|(webOS)/i)==null?'desktop':'mobile',
+			
+			browser: navigator.userAgent.match(/(opera|chrome|safari|firefox|msie|trident)\/?\s*([\d\.]+)/i)[1],
+			
+			version: navigator.userAgent.match(/(opera|chrome|safari|firefox|msie|trident)\/?\s*([\d\.]+)/i)[2]
+		
+		},
+		
 		get: function( key ){
 			
 			return this.params[key];
