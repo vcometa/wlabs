9204d4f885d8e7810b952cd93c5ae3dfc2534c8b
vcometa <vcometa@postmedia.com>
2013-12-09 16:54:04 -0500

    update

--- widgets/toolset.js
+++ widgets/toolset.js
@@ -16,172 +16,7 @@
 
 	$.fn.uxToolSet = function(params) {
 		
-		/*
-		* params: width, height, barColor, barHeight, tabWidth, tabHeight, leftColor, rightColor
-		*/
-
-		function rangeSlider( objectSelector, outputSelector ){
-
-			var r = new Object();
-				r.range = $(objectSelector);
-				r.output = $(outputSelector);
-				r.type = r.range.data('type');
-				
-				
-			var html = '<button class="left-handle" data-type="min"></button><button class="right-handle" data-type="max"></button><div class="cover-left"></div><div class="cover-right"></div><div class="slider-bar"></div>';	
-			
-			if( r.type == 'single'){
-			
-				html = '<button class="right-handle" data-type="max"></button><div class="cover-right"></div><div class="slider-bar"></div>';
-				
-			}
-			
-			$(html).appendTo(r.range);		
-			r.button = $( r.range.find('button') );
-			r.sBar = $(r.range.find('.slider-bar'));
-			r.coverLeft = $( r.range.find('.cover-left'));	
-			r.coverRight = $( r.range.find('.cover-right'));
-			r.target = null;
-			r.dragging = false;
-			r.rLim = r.sBar.outerWidth() - r.button.outerWidth();
-			r.outerLim = r.sBar.outerWidth() + r.button.outerWidth();
-			r.minVal = parseInt( r.range.data('min') );
-			r.maxVal = parseInt( r.range.data('max') );
-			r.sMinVal = parseInt( r.range.data('start-min') );
-			r.sMaxVal = parseInt( r.range.data('start-max') );
-			r.retMin = r.minVal;
-			r.retMax = r.maxVal;
-			r.lastPos = 0;
-			r.interval = 0.01;
-			
-			var getPos = function( val ){
-				
-				val = (val/r.maxVal)*r.rLim;
-			
-				return (val + document.body.scrollLeft - document.body.clientLeft)-r.button.width();
-			
-			}
-			
-			
-			
-			if( r.sMinVal && r.sMaxVal){
-			
-				var lPos = getPos(r.sMinVal),
-					rPos =  getPos(r.sMaxVal);
-			
-				$(r.button[0]).css('left', lPos );
-				r.coverLeft.width( lPos );
-				
-				$(r.button[1]).css('left', rPos );
-				r.coverRight.width( r.rLim - rPos );
-				
-				r.retMin = r.sMinVal;
-				r.retMax = r.sMaxVal;
-				
-				r.output.val( r.sMinVal+' to '+ r.sMaxVal );
-			
-			} else if( !r.sMinVal && r.sMaxVal){
-			
-				var rPos =  getPos(r.sMaxVal);
-				
-				$(r.button[0]).css('left', rPos );
-				r.coverRight.width( r.rLim - rPos );
-				
-				r.retMax = r.sMaxVal;
-				
-				r.output.val( r.minVal+' to '+ r.sMaxVal );
-			
-			} else {	
-			
-				r.output.val( r.minVal+' to '+ r.maxVal );	
-			
-			}
-			
-			r.button.on('mousedown touchstart', function(e){
-			
-				e.preventDefault();
-				
-				r.target = $( this );
-				
-				startX = (e.clientX != null)?e.clientX:event.touches[0].pageX;
-						
-				if( startX <= r.outerLim && startX >= 0){
-				
-					r.dragging = true;
-				
-				}
-			
-			});
-
-			$(document).on('mousemove touchmove', function(e){
-			
-				var x = (e.clientX != null)?e.clientX:event.touches[0].pageX;
-			
-				if( r.dragging && x <= r.outerLim && x >= 0){
-				
-					e.preventDefault();
-				
-					var pos = ((x + document.body.scrollLeft - document.body.clientLeft)-r.button.width())<0?0:(x + document.body.scrollLeft - document.body.clientLeft)-r.button.width(),
-					
-						isMin = ( r.target.data('type') == 'min' )?true:false,
-						
-						offset = ( r.type == 'double')?( r.maxVal * r.interval ):0,
-						
-						lTemp = r.retMin+offset,
-						
-						rTemp = r.retMax-offset;
-						
-					if( (r.retMin+offset < r.retMax-offset) ||  ( isMin && r.lastPos > pos ) || ( !isMin && r.lastPos < pos ) ){
-							
-						if( pos <= r.rLim ){
-							
-							var retVal = Math.round(r.maxVal * (pos/r.rLim));
-						
-							r.target.css('left', pos );
-								
-							if ( isMin ){
-							
-								r.retMin = retVal;
-								
-								r.coverLeft.width( pos );
-								
-							} else {
-							
-								r.retMax = retVal;
-								
-								r.coverRight.width( r.rLim - pos );
-							
-							}
-							
-							r.lastPos = pos;
-						
-						} else if( pos > r.rLim ){				
-							
-							r.retMax = r.maxVal;
-							
-							r.target.css('left', r.rLim );
-						
-						}
-						
-						r.output.val( r.retMin+' to '+ r.retMax );				
-				
-					}
-				
-				}
-			
-			});
-
-			$(document).on('mouseup touchend', function(){
-			
-				if( r.dragging ){
-				 
-					r.dragging = false;
-				
-				}
-			
-			});
-
-		}
+		
 
 		function addCSS(strCSS) {
 			var node = document.createElement('style');
@@ -189,36 +24,205 @@
 			document.head.appendChild(node);
 		}
 		
-		function getParams(){
+		/** Range Slider begins **/
+
+		function setRangeSlider(){
 		
-			var defaultParams = {'width':'300px', 'height':'30px', 'barColor':'#333', 'barHeight':'10px', 'tabWidth':'10px', 'tabHeight':'30px', 'leftColor':'red', 'rightColor':'blue'};
+			var getRangeSliderParams = function(){
 		
-			if(params != null){
-				
-				for (var key in defaultParams) {
+				var defaultParams = {'width':'300px', 'height':'30px', 'barColor':'#333', 'barHeight':'10px', 'tabWidth':'10px', 'tabHeight':'30px', 'leftColor':'red', 'rightColor':'blue'};
+			
+				if(params != null){
 					
-					if( params[key] != defaultParams[key] ){
-					
-						if( params[key] != null ){
+					for (var key in defaultParams) {
 						
-							defaultParams[key] = params[key];
+						if( params[key] != defaultParams[key] ){
+						
+							if( params[key] != null ){
+							
+								defaultParams[key] = params[key];
+							}
 						}
 					}
+				
+					return defaultParams;
+				
+				}else{
+				
+					return defaultParams;
+				
 				}
 			
-				return defaultParams;
+			},
 			
-			}else{
-			
-				return defaultParams;
-			
-			}
-		
-		}
+			/*
+			* params: width, height, barColor, barHeight, tabWidth, tabHeight, leftColor, rightColor
+			*/
 
-		function init(){
+			rangeSlider = function( objectSelector, outputSelector ){
+
+				var r = new Object();
+					r.range = $(objectSelector);
+					r.output = $(outputSelector);
+					r.type = r.range.data('type');
+					
+					
+				var html = '<button class="left-handle" data-type="min"></button><button class="right-handle" data-type="max"></button><div class="cover-left"></div><div class="cover-right"></div><div class="slider-bar"></div>';	
+				
+				if( r.type == 'single'){
+				
+					html = '<button class="right-handle" data-type="max"></button><div class="cover-right"></div><div class="slider-bar"></div>';
+					
+				}
+				
+				$(html).appendTo(r.range);		
+				r.button = $( r.range.find('button') );
+				r.sBar = $(r.range.find('.slider-bar'));
+				r.coverLeft = $( r.range.find('.cover-left'));	
+				r.coverRight = $( r.range.find('.cover-right'));
+				r.target = null;
+				r.dragging = false;
+				r.rLim = r.sBar.outerWidth() - r.button.outerWidth();
+				r.outerLim = r.sBar.outerWidth() + r.button.outerWidth();
+				r.minVal = parseInt( r.range.data('min') );
+				r.maxVal = parseInt( r.range.data('max') );
+				r.sMinVal = parseInt( r.range.data('start-min') );
+				r.sMaxVal = parseInt( r.range.data('start-max') );
+				r.retMin = r.minVal;
+				r.retMax = r.maxVal;
+				r.lastPos = 0;
+				r.interval = 0.01;
+				
+				var getPos = function( val ){
+					
+					val = (val/r.maxVal)*r.rLim;
+				
+					return (val + document.body.scrollLeft - document.body.clientLeft)-r.button.width();
+				
+				}
+				
+				
+				
+				if( r.sMinVal && r.sMaxVal){
+				
+					var lPos = getPos(r.sMinVal),
+						rPos =  getPos(r.sMaxVal);
+				
+					$(r.button[0]).css('left', lPos );
+					r.coverLeft.width( lPos );
+					
+					$(r.button[1]).css('left', rPos );
+					r.coverRight.width( r.rLim - rPos );
+					
+					r.retMin = r.sMinVal;
+					r.retMax = r.sMaxVal;
+					
+					r.output.val( r.sMinVal+' to '+ r.sMaxVal );
+				
+				} else if( !r.sMinVal && r.sMaxVal){
+				
+					var rPos =  getPos(r.sMaxVal);
+					
+					$(r.button[0]).css('left', rPos );
+					r.coverRight.width( r.rLim - rPos );
+					
+					r.retMax = r.sMaxVal;
+					
+					r.output.val( r.minVal+' to '+ r.sMaxVal );
+				
+				} else {	
+				
+					r.output.val( r.minVal+' to '+ r.maxVal );	
+				
+				}
+				
+				r.button.on('mousedown touchstart', function(e){
+				
+					e.preventDefault();
+					
+					r.target = $( this );
+					
+					startX = (e.clientX != null)?e.clientX:event.touches[0].pageX;
+							
+					if( startX <= r.outerLim && startX >= 0){
+					
+						r.dragging = true;
+					
+					}
+				
+				});
+
+				$(document).on('mousemove touchmove', function(e){
+				
+					var x = (e.clientX != null)?e.clientX:event.touches[0].pageX;
+				
+					if( r.dragging && x <= r.outerLim && x >= 0){
+					
+						e.preventDefault();
+					
+						var pos = ((x + document.body.scrollLeft - document.body.clientLeft)-r.button.width())<0?0:(x + document.body.scrollLeft - document.body.clientLeft)-r.button.width(),
+						
+							isMin = ( r.target.data('type') == 'min' )?true:false,
+							
+							offset = ( r.type == 'double')?( r.maxVal * r.interval ):0,
+							
+							lTemp = r.retMin+offset,
+							
+							rTemp = r.retMax-offset;
+							
+						if( (r.retMin+offset < r.retMax-offset) ||  ( isMin && r.lastPos > pos ) || ( !isMin && r.lastPos < pos ) ){
+								
+							if( pos <= r.rLim ){
+								
+								var retVal = Math.round(r.maxVal * (pos/r.rLim));
+							
+								r.target.css('left', pos );
+									
+								if ( isMin ){
+								
+									r.retMin = retVal;
+									
+									r.coverLeft.width( pos );
+									
+								} else {
+								
+									r.retMax = retVal;
+									
+									r.coverRight.width( r.rLim - pos );
+								
+								}
+								
+								r.lastPos = pos;
+							
+							} else if( pos > r.rLim ){				
+								
+								r.retMax = r.maxVal;
+								
+								r.target.css('left', r.rLim );
+							
+							}
+							
+							r.output.val( r.retMin+' to '+ r.retMax );				
+					
+						}
+					
+					}
+				
+				});
+
+				$(document).on('mouseup touchend', function(){
+				
+					if( r.dragging ){
+					 
+						r.dragging = false;
+					
+					}
+				
+				});
+
+			};
 		
-			params = getParams();			
+			params = getRangeSliderParams();			
 
 			var rangeArr = $('.range-slider'),
 				rangeCSS = "@media all{.range-slider{position:relative;width:"+params.width+";height:"+params.height+";overflow:hidden}.range-slider .slider-bar{position:absolute;top:10px;left:0;background:"+params.barColor+";width:100%;height:"+params.barHeight+"}.range-slider .cover-left,.range-slider .cover-right{position:relative;margin:10px auto;float:left;z-index:1;background:#ccc;width:10px;height:10px}.range-slider .cover-right{float:right}.range-slider button{position:absolute;top:0;z-index:9;width:"+params.tabWidth+";height:"+params.tabHeight+";border:0;padding:0;margin:0;cursor:pointer;outline:0}.range-slider .left-handle{left:0;background:"+params.leftColor+"}.range-slider .right-handle{right:0;background:"+params.rightColor+"}}@media all and (max-width:767px){.range-slider button{width:44px;height:44px}}";
@@ -239,9 +243,16 @@
 			}
 
 		}
-
-		init();
 		
+		/** Range Slider ends **/
+		
+		function init(){
+
+			setRangeSlider();
+		
+		}
+		
+		init();
 		return this;
 
 	};
