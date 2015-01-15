08028f41638d3ce3a5816dc321247697bcb8b774
vcometa <vcometa@postmedia.com>
2015-01-15 17:15:24 -0500

    updates

--- page.php
+++ page.php
@@ -19,16 +19,19 @@
 
 		while ( $db_field = mysqli_fetch_assoc($result) ) {
 			
-			$contentBlock = '<h1>'.html_entity_decode($db_field['title']).'</h1>'.
-			'<h2>'.html_entity_decode($db_field['description']).'</h2>'.
+			$articleTags = html_entity_decode($db_field['tags']);
+			$articleDesc = html_entity_decode($db_field['description']);
+			$articleTitle = html_entity_decode($db_field['title']);
+			
+			$contentBlock = '<h1>'.$articleTitle.'</h1>'.
+			'<h2>'.$articleDesc.'</h2>'.
 			'<figure><img src="/images/photos/'.html_entity_decode($db_field['imgname']).'" title="'.html_entity_decode($db_field['articlename']).'"/></figure>'.			
 			'<div class="source">'.html_entity_decode($db_field['author']).'</div>'.
 			'<a class="source" href="'.html_entity_decode($db_field['source']).'">'.html_entity_decode($db_field['sourcename']).'</a>'.
 			'<article>'.html_entity_decode($db_field['article']) . '</article>'.
 			'<div class="tags">'.$articleTags. '</div>';
 			
-			$articleTags = html_entity_decode($db_field['tags']);
-			$articleDesc = html_entity_decode($db_field['description']);			
+			
 
 		}
 		mysqli_close($db_handle);
@@ -70,6 +73,18 @@
 
 	<div class="content">
 		<div class="left-rail">
+		
+			<div class="socialmedia">
+				<ul>
+					<li class="facebook">
+						<a title="facebook" target="_blank" href="http://www.facebook.com/sharer.php?u=http%3A%2F%2Fpinstacular.com%2Farticle%2F<?PHP echo $articlename ?>&amp;t=<?PHP echo $articleTitle ?>"></a>
+					</li>
+					<li class="twitter"><a title="twitter" target="_blank" href="https://twitter.com/intent/tweet?text=Land+Rover+gives+closer+look+at+upcoming+Discovery%C2%A0Sport - http://driving.wpdev5.canada.com/land-rover/auto-news/news/land-rover-gives-closer-look-at-upcoming-discovery-sport - via @drivingdotca"></a></li>
+					<li class="pinterest"><a class="addthis_button_pinterest_pinit"><span class="at_PinItButton"></span></a></li>
+					<li class="email"><a class="addthis_button_email" target="_blank" title="Email" href="#"></a></li>
+				</ul>
+			</div>
+			
 			<?PHP echo $contentBlock ?>
 			<div class="fb-like" data-href="https://developers.facebook.com/docs/plugins/" data-layout="button" data-action="like" data-show-faces="true" data-share="true"></div>
 			<div class="fb-comments" data-href="<?php echo 'http://pinstacular.com/article/'.html_entity_decode($articlename) ?>" data-width="100%" data-numposts="7" data-colorscheme="light"></div>
